88dc3792a62d5d7602027b4f5726bdc6623f9736
daveclinton <clintondavid46@gmail.com>
2023-04-28 19:42:20 +0300

    Fix:Change admin section to functinal component

--- pages/admin.js
+++ pages/admin.js
@@ -1,67 +1,86 @@
-import React from 'react'
-import SignUp from '../components/formComponents/SignUp'
-import ConfirmSignUp from '../components/formComponents/ConfirmSignUp'
-import SignIn from '../components/formComponents/SignIn'
-import Inventory from '../components/Inventory'
+import React, { useState, useEffect } from "react"
+import SignUp from "../components/formComponents/SignUp"
+import ConfirmSignUp from "../components/formComponents/ConfirmSignUp"
+import SignIn from "../components/formComponents/SignIn"
+import Inventory from "../components/Inventory"
 
-class Admin extends React.Component {
-  state = { formState: 'signUp', isAdmin: false }
-  toggleFormState = (formState) => {
-    this.setState(() => ({ formState }))
+const Admin = () => {
+  const [formState, setFormState] = useState("signUp")
+  const [isAdmin, setIsAdmin] = useState(false)
+
+  const toggleFormState = (formState) => {
+    setFormState(formState)
   }
-  async componentDidMount() {
+
+  useEffect(() => {
     // check and update signed in state
-  }
-  signUp = async (form) => {
+  }, [])
+
+  const signUp = async (form) => {
     const { username, email, password } = form
     // sign up
-    this.setState({ formState: 'confirmSignUp' })
-  }
-  confirmSignUp = async (form) => {
-    const { username, authcode } = form
-    // confirm sign up
-    this.setState({ formState: 'signIn' })
-  }
-  signIn = async (form) => {
-    const { username, password } = form
-    // signIn
-    this.setState({ formState: 'signedIn', isAdmin: true })
-  }
-  signOut = async() => {
-    // sign out
-    this.setState({ formState: 'signUp' })
+    setFormState("confirmSignUp")
   }
 
-  render() {
-    const { formState, isAdmin } = this.state
-    const renderForm = (formState, state) => {
-      switch(formState) {
-        case 'signUp':
-          return <SignUp {...state} signUp={this.signUp} toggleFormState={this.toggleFormState} />
-        case 'confirmSignUp':
-          return <ConfirmSignUp {...state} confirmSignUp={this.confirmSignUp} />
-        case 'signIn':
-          return <SignIn {...state} signIn={this.signIn} toggleFormState={this.toggleFormState} />
-        case 'signedIn':
-          return isAdmin ? <Inventory {...state} signOut={this.signOut} /> : <h3>Not an admin</h3>
-        default:
-          return null
-      }
-    }
-    
-    return (
-      <div className="flex flex-col">
-        <div className="max-w-fw flex flex-col">
-          <div className="pt-10">
-            <h1 className="text-5xl font-light">Admin Panel</h1>
-          </div>
-          {
-            renderForm(formState)
-          }
-        </div>
-      </div>
-    )
+  const confirmSignUp = async (form) => {
+    const { username, authcode } = form
+    // confirm sign up
+    setFormState("signIn")
   }
+
+  const signIn = async (form) => {
+    const { username, password } = form
+    // signIn
+    setFormState("signedIn")
+    setIsAdmin(true)
+  }
+
+  const signOut = async () => {
+    // sign out
+    setFormState("signUp")
+  }
+
+  const renderForm = (formState, state) => {
+    switch (formState) {
+      case "signUp":
+        return (
+          <SignUp
+            {...state}
+            signUp={signUp}
+            toggleFormState={toggleFormState}
+          />
+        )
+      case "confirmSignUp":
+        return <ConfirmSignUp {...state} confirmSignUp={confirmSignUp} />
+      case "signIn":
+        return (
+          <SignIn
+            {...state}
+            signIn={signIn}
+            toggleFormState={toggleFormState}
+          />
+        )
+      case "signedIn":
+        return isAdmin ? (
+          <Inventory {...state} signOut={signOut} />
+        ) : (
+          <h3>Not an admin</h3>
+        )
+      default:
+        return null
+    }
+  }
+
+  return (
+    <div className="flex flex-col">
+      <div className="max-w-fw flex flex-col">
+        <div className="pt-10">
+          <h1 className="text-5xl font-light">Admin Panel</h1>
+        </div>
+        {renderForm(formState)}
+      </div>
+    </div>
+  )
 }
 
 export default Admin
