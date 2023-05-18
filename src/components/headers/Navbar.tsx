11ae5b6dfde8c8219a069e4ec4249e7afb33b20e
daveclinton <clintondavid46@gmail.com>
2023-05-18 09:29:12 +0300

    Feat: Add component for mobile drawer

--- src/components/headers/Navbar.tsx
+++ src/components/headers/Navbar.tsx
@@ -1,28 +1,13 @@
 import * as React from "react";
-import { Flex, Heading, Link, Divider } from "@chakra-ui/layout";
+import { Flex, Heading, Link } from "@chakra-ui/layout";
 import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/menu";
-import {
-  ChevronDownIcon,
-  HamburgerIcon,
-  CloseIcon,
-  MinusIcon,
-  AddIcon,
-} from "@chakra-ui/icons";
-import { Drawer, DrawerContent, DrawerBody } from "@chakra-ui/modal";
+import { ChevronDownIcon, HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { Button, IconButton } from "@chakra-ui/button";
 import { Avatar } from "@chakra-ui/avatar";
 import { Input } from "@chakra-ui/input";
 import { GiShoppingCart } from "react-icons/gi";
-import { TbDiscount2 } from "react-icons/tb";
-import { MdOutlineShoppingCart } from "react-icons/md";
 import { useDisclosure, useMediaQuery, Icon } from "@chakra-ui/react";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-} from "@chakra-ui/accordion";
-import { categories } from "../../utils/ProductCategories";
+import MobileMenu from "./MobileMenu";
 
 const Navbar: React.FC = () => {
   const [isLargerThanMd] = useMediaQuery("(min-width: 740px)");
@@ -123,128 +108,7 @@ const Navbar: React.FC = () => {
               placeholder="Search for products, shops and categories"
             />
           </Flex>
-          <Drawer
-            size="md"
-            placement="left"
-            isFullHeight={false}
-            onClose={onClose}
-            isOpen={isOpen}
-            variant="primary"
-          >
-            <DrawerContent>
-              <Flex
-                h="96px"
-                justify="space-evenly"
-                align="center"
-                borderBottomWidth="2px"
-              >
-                <Flex justify="center" align="center" flexDir="column">
-                  <IconButton
-                    boxSize="40px"
-                    variant="unstyled"
-                    cursor="pointer"
-                    aria-label=""
-                    as={TbDiscount2}
-                  />
-                  <Link _hover={{ textDecor: "none", color: "primaryYellow" }}>
-                    Deals & Promos
-                  </Link>
-                </Flex>
-                <Divider orientation="vertical" w="2px" />
-                <Flex justify="center" align="center" flexDir="column">
-                  <IconButton
-                    boxSize="40px"
-                    variant="unstyled"
-                    cursor="pointer"
-                    aria-label=""
-                    as={MdOutlineShoppingCart}
-                  />
-                  <Link _hover={{ textDecor: "none", color: "primaryYellow" }}>
-                    Open a Shop?
-                  </Link>
-                </Flex>
-              </Flex>
-              <DrawerBody>
-                <Accordion allowToggle>
-                  {categories.map(
-                    ({ name, icon, subcategories }, accordionData) => (
-                      <AccordionItem key={accordionData} border="none">
-                        {({ isExpanded }) => (
-                          <>
-                            <AccordionButton>
-                              <Flex
-                                as="span"
-                                align="center"
-                                gap="20px"
-                                flex="1"
-                                textAlign="left"
-                              >
-                                <Icon
-                                  as={icon}
-                                  color="primaryYellow"
-                                  boxSize="20px"
-                                />
-                                <Link
-                                  _hover={{
-                                    textDecoration: "none",
-                                    color: "primaryYellow",
-                                  }}
-                                  _active={{
-                                    textDecoration: "none",
-                                    color: "primaryYellow",
-                                  }}
-                                >
-                                  {name}
-                                </Link>
-                              </Flex>
-                              {isExpanded ? (
-                                <MinusIcon fontSize="12px" />
-                              ) : (
-                                <AddIcon fontSize="12px" />
-                              )}
-                            </AccordionButton>
-                            {subcategories ? (
-                              <AccordionPanel
-                                display="flex"
-                                ml="20px"
-                                flexDir="column"
-                              >
-                                {subcategories.map((subcategory, subIndex) => (
-                                  <Link
-                                    key={subIndex}
-                                    ml={8}
-                                    py={1}
-                                    _hover={{
-                                      textDecoration: "none",
-                                      color: "primaryYellow",
-                                    }}
-                                    _active={{
-                                      textDecoration: "none",
-                                      color: "primaryYellow",
-                                    }}
-                                    href={subcategory.link}
-                                  >
-                                    {subcategory.name}
-                                  </Link>
-                                ))}
-                              </AccordionPanel>
-                            ) : (
-                              <AccordionPanel
-                                display="flex"
-                                justifyContent="center"
-                              >
-                                Items Out Of Stock
-                              </AccordionPanel>
-                            )}
-                          </>
-                        )}
-                      </AccordionItem>
-                    )
-                  )}
-                </Accordion>
-              </DrawerBody>
-            </DrawerContent>
-          </Drawer>
+          <MobileMenu isOpen={isOpen} onClose={onClose} />
         </>
       )}
     </Flex>
