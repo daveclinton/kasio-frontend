d70025f1ab83426b2521edf3716202b2e2ca16f0
daveclinton <clintondavid46@gmail.com>
2023-05-19 22:08:09 +0300

    Feat: Add bottom navbar

--- src/components/headers/LowerMenu.tsx
+++ src/components/headers/LowerMenu.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, IconButton, Text } from "@chakra-ui/react";
 import { ChevronDownIcon, Icon } from "@chakra-ui/icons";
 import { categories } from "../../utils/ProductCategories";
 
@@ -13,13 +13,17 @@ const LowerMenu: React.FC = () => {
       minH="72px"
       color="white"
       bg="#231f20"
-      gap="10px"
+      gap="20px"
     >
       {categories.map(({ name, icon }, menuData) => (
         <Flex align="center" key={menuData} gap="10px">
           <Icon as={icon} color="primaryYellow" boxSize="20px" />
           <Text>{name}</Text>
-          <ChevronDownIcon ml="10px" />
+          <IconButton
+            variant="unstyled"
+            aria-label=""
+            icon={<ChevronDownIcon ml="10px" />}
+          />
         </Flex>
       ))}
     </Flex>
