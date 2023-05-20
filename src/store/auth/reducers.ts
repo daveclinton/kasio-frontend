4e446f17980f58d363415b6a12da70e9b9f42973
daveclinton <clintondavid46@gmail.com>
2023-05-20 12:52:41 +0300

    Feat: Add authreducer

--- src/store/auth/reducers.ts
+++ src/store/auth/reducers.ts
@@ -1,10 +1,20 @@
 import { SET_AUTH_USER, CLEAR_AUTH_STORE } from "./types";
 
+interface User {
+  name: string;
+  email: string;
+}
+
+interface AuthAction {
+  type: string;
+  payload: User;
+}
+
 const initialState = {
   user: null,
 };
 
-const authReducer = (state = initialState, action: any) => {
+const authReducer = (state = initialState, action: AuthAction) => {
   switch (action.type) {
     case SET_AUTH_USER:
       return {
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import reducers from "./translations/reducers";
+import authReducer from "./auth/reducers";
 
 const rootReducer = combineReducers({
   translates: reducers,
+  authReducer,
 });
 
 export default rootReducer;
