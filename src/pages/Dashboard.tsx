cdd995be07fecfb2c05eb9b6a1dec5e0c8cb690b
daveclinton <clintondavid46@gmail.com>
2023-05-20 13:20:58 +0300

    Feat: Add user state

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
+import { useAppSelector } from "../store/hooks";
+import { Box } from "@chakra-ui/react";
 
 const Dashboard: React.FC = () => {
-  return <div></div>;
+  const { user } = useAppSelector(({ authReducer }) => authReducer);
+
+  return <Box>Hello</Box>;
 };
 
 export default Dashboard;
