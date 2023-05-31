2b13d9c9531e9dffd8074ba26d08570abd54a8fe
daveclinton <clintondavid46@gmail.com>
2023-05-31 09:22:56 +0300

    Feat: Add footer mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Flex } from "@chakra-ui/layout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import MainHeader from "./components/headers/MainHeader";
 import Navbar from "./components/headers/Navbar";
 import LowerMenu from "./components/headers/LowerMenu";
@@ -13,6 +13,13 @@ import Dashboard from "./pages/Dashboard";
 import Footer from "./components/headers/Footer";
 
 const App: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  if (location.pathname === "/") {
+    navigate("/home", { replace: true });
+  }
+
   return (
     <Flex align="center" justify="center" w="100%">
       <Flex w="100%" flexDir="column">
@@ -22,7 +29,7 @@ const App: React.FC = () => {
         <Box mt="210px">
           <Routes>
             <Route>
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/home" element={<Dashboard />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/check-email" element={<CheckEmail />} />
--- src/components/headers/Footer.tsx
+++ src/components/headers/Footer.tsx
@@ -14,12 +14,18 @@ const Footer: React.FC = () => {
       width="100%"
       bg="#0A142F"
       color="white"
-      h="490px"
+      h={{ lg: "490px" }}
       left={0}
       bottom={0}
     >
-      <Flex w="100%%" pt="100px" flexDirection="column">
-        <Flex m="auto" gap="100px" borderBottom="2px solid #fff">
+      <Flex w="100%" pt="100px" flexDirection="column">
+        <Flex
+          m="auto"
+          justify={{ base: "center", lg: "auto" }}
+          flexWrap="wrap"
+          gap={{ base: "50px", lg: "100px" }}
+          borderBottom="2px solid #fff"
+        >
           <Box>
             <Heading mb="40px">KASIO</Heading>
             <Text mb="15px">+254 720 000 185</Text>
@@ -38,7 +44,7 @@ const Footer: React.FC = () => {
             <Text mb="15px">+254 720 000 185</Text>
             <Text mb="15px">kasio@market.com</Text>
           </Box>
-          <Box>
+          <Box pb={{ base: "30px" }}>
             <Heading fontSize="18px">Subscribe</Heading>
             <InputGroup mt="20px" size="sm">
               <Input placeholder="Get Product Updates" />
@@ -55,7 +61,14 @@ const Footer: React.FC = () => {
             </InputGroup>
           </Box>
         </Flex>
-        <Flex justify="space-between" gap="170px" pt="50px" m="auto">
+        <Flex
+          flexDir={{ base: "column", lg: "row" }}
+          justify={{ base: "center", lg: "space-between" }}
+          align="center"
+          gap={{ base: "30px", lg: "170px" }}
+          pt="50px"
+          m="auto"
+        >
           <Flex justify="space-between" gap="30px">
             <Icon as={CiLinkedin} boxSize="30px" />
             <Icon as={MdOutlineFacebook} boxSize="30px" />
