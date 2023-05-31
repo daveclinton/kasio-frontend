9a69365909896cf43903fb3053d1153241767d7a
daveclinton <clintondavid46@gmail.com>
2023-05-31 10:59:28 +0300

    Feat: Add category design

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,11 +1,43 @@
 import * as React from "react";
-import { Box } from "@chakra-ui/layout";
+import { Box, Heading, Flex, Stack, Text } from "@chakra-ui/layout";
+import { Radio } from "@chakra-ui/radio";
 import BreadCrumb from "../components/BreadCrumb";
 
 const Categories: React.FC = () => {
   return (
-    <Box>
-      <BreadCrumb />
+    <Box mt="-30px">
+      <Box pl="5%" pt="10px" h="50px" bg="#fafafa">
+        <BreadCrumb />
+      </Box>
+      <Heading
+        textAlign={{ base: "center", lg: "justify" }}
+        fontSize="35px"
+        fontWeight={700}
+        pl={{ base: "auto", lg: "5%" }}
+      >
+        Printers & Scanners
+      </Heading>
+      <Flex justify="center" align="center" mt="30px" w="90%">
+        <Flex gap="20%">
+          <Box>
+            <Stack>
+              <Radio size="md" name="1" colorScheme="yellow">
+                Products{4405}
+              </Radio>
+              <Radio size="md" name="1" colorScheme="yellow">
+                Shops{100}
+              </Radio>
+            </Stack>
+          </Box>
+          <Flex align="center" flexDir="column">
+            <Text textAlign="left">
+              A wide range of high quality printers and scanners that include
+              Epson, Canon Pixma, HP, Kyocera among others for your home, office
+              or business. Delivery is in Nairobi and all over Kenya.
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
     </Box>
   );
 };
