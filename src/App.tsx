f9c240ce9521e9e3bbf9059b078f1bd1cef91579
daveclinton <clintondavid46@gmail.com>
2023-06-07 14:14:05 +0300

    Fix: Add fix for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Flex } from "@chakra-ui/layout";
-import { Routes, Route, useLocation, redirect } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainHeader from "./components/headers/MainHeader";
 import Navbar from "./components/headers/Navbar";
 import LowerMenu from "./components/headers/LowerMenu";
@@ -14,13 +14,6 @@ import Footer from "./components/headers/Footer";
 import Categories from "./pages/Categories";
 
 const App: React.FC = () => {
-  const location = useLocation();
-  const { pathname } = location;
-
-  if (pathname === "/") {
-    redirect("/home");
-  }
-
   return (
     <Flex align="center" justify="center" w="100%">
       <Flex w="100%" flexDir="column">
@@ -29,7 +22,7 @@ const App: React.FC = () => {
         <LowerMenu />
         <Box mt="210px">
           <Routes>
-            <Route path="/home" element={<Dashboard />} />
+            <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/check-email" element={<CheckEmail />} />
@@ -37,7 +30,7 @@ const App: React.FC = () => {
             <Route path="/category" element={<Categories />} />
           </Routes>
         </Box>
-        {pathname === "/home" && <Footer />}
+        <Footer />
       </Flex>
     </Flex>
   );
