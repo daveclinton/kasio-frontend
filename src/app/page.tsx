14bdc19f9ff9ca194b3bc562474d2637910ec8d9
daveclinton <clintondavid46@gmail.com>
2023-07-30 22:40:02 +0300

    feat: Add changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
-}
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
