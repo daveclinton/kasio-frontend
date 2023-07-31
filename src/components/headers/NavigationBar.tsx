499a20291805f7b84966896df1636b7b5c728a6e
daveclinton <clintondavid46@gmail.com>
2023-07-31 22:27:26 +0300

    feat: Add my acccount details

--- src/components/headers/NavigationBar.tsx
+++ src/components/headers/NavigationBar.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
-import { Box, Center, Text } from "@chakra-ui/layout";
+import { Box, Center, Text, Flex } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
+import { Icon } from "@chakra-ui/icon";
+import { TbTruckDelivery } from "react-icons/tb";
+import { BsInstagram, BsFacebook, BsTwitter } from "react-icons/bs";
 
 const NavigationBar: React.FC = () => {
   return (
@@ -17,6 +20,28 @@ const NavigationBar: React.FC = () => {
         >
           SHOP NOW!
         </Button>
+      </Center>
+      <Center w="100%">
+        <Center maxW="80%" w="100%" justifyContent="space-between">
+          <Flex align="center">
+            <Icon as={TbTruckDelivery} color="primaryYellow" />
+            <Text fontSize="13px">
+              FREE Delivery in Migori On Orders Ksh.5,000+
+            </Text>
+          </Flex>
+          <Flex align="center" fontSize="14px" fontWeight={400} gap="20px">
+            <Flex gap="20px">
+              <Text>My Account</Text>
+              <Text>Cart</Text>
+              <Text>Login</Text>
+            </Flex>
+            <Flex gap="20px">
+              <Icon as={BsInstagram} color="primaryYellow" />
+              <Icon as={BsFacebook} color="primaryYellow" />
+              <Icon as={BsTwitter} color="primaryYellow" />
+            </Flex>
+          </Flex>
+        </Center>
       </Center>
     </Box>
   );
